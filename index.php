c7f5ac6acf1de2f8c92bcd507a327d1e6fcb8d9b
Deuklyoung Ko <deutschgon@gmail.com>
2018-09-13 05:31:25 +0000

    add plugins

---
+++ index.php
@@ -0,0 +1,7 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+      <meta http-equiv="refresh" content="0;url=https://www.dgon.eu/wp/wordpress"/>
+      <title></title>
+  </head>
+  <body></body>
+</html>
