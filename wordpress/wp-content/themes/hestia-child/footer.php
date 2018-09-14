bfc7ef0e62d52a75b2ddb0a483bbc8d1eada2da0
Deuklyoung Ko <deutschgon@gmail.com>
2018-09-14 23:50:17 +0000

    regular backup

---
+++ wordpress/wp-content/themes/hestia-child/footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "wrapper" div and all content after.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+?>
+            <footer class="footer footer-black footer-big">
+            	<div class="container">
+            		<div class="hestia-bottom-footer-content">
+                        <ul class="footer-menu pull-left">
+                            <li class="page_item page-item-192 current_page_item">Copyright 2018 Deuklyoung Ko All rights reserved.</li>
+                        </ul>
+            			<div class="copyright pull-right">
+                            <ul class="list-inline social-buttons">
+                              <li class="list-inline-item">
+                                <a href="https://www.linkedin.com/in/dgon/?locale=en_US" target="_blank">
+                                  <i class="fa fa-linkedin fa-2x"></i>
+                                </a>
+                              </li>
+                              <li class="list-inline-item">
+                                <a href="https://www.xing.com/profile/Deuklyoung_Ko/cv?sc_o=mxb_p" target="_blank">
+                                  <i class="fa fa-xing-square fa-2x"></i>
+                                </a>
+                              </li>
+                            </ul>
+                        </div>
+            		</div>
+                </div>
+            </footer>
+
+		</div>
+	</div>
+<?php wp_footer(); ?>
+</body>
+</html>
