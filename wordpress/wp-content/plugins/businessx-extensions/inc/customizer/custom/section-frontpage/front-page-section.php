a12600c5cc2a17b569ff080d64e8d158e424643c
Deuklyoung Ko <deutschgon@gmail.com>
2018-09-15 06:19:34 +0000

    ver 1.1

--- wordpress/wp-content/plugins/businessx-extensions/inc/customizer/custom/section-frontpage/front-page-section.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Front Page section
- */
-if( ! class_exists( 'BXEXT_Section_FrontPage' ) ) {
-	class BXEXT_Section_FrontPage extends WP_Customize_Section {
-		public $type = 'front-page';
-	}
-}
